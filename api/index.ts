a603fe627619fe8fcc9b5fda82ede183982dd632
Jonathan_Morales <jmoralesc54@miumg.edu.gt>
2025-10-15 01:53:08 -0600

    Correcciones para deploy y agregados objetos de build al gitignore

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import app from '../src/app.js';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import app from '../src/app';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   return app(req, res);
